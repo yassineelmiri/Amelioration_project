7744d3426014785ed84dd7b4b9fffcd637cefffc
YASSINE DEV <yassireelhima@gmail.com>
2023-12-19 17:19:06 +0100

    apre la parti de B-END je style avec une fromwork

---
+++ public/partials/_footer.php
@@ -0,0 +1,4 @@
+<div class="topspace"></div>
+<footer>
+    <p>&copy 2024 Made with ❤️ By <a href="#" target="black"><strong>yassine elmiri</strong></a> 
+</footer>
---
+++ public/partials/_navbar.php
@@ -0,0 +1,24 @@
+<?php
+
+    echo'<div class="nevbar" id="nevbar">
+    <div class="logo">
+        <a href="../login/index.php" id="logo"><h1><span>CIH</span> BANK</h1></a>
+    </div>
+    <div class="menu" id="menu"> 
+        <ul>
+            <li><a id="home" href="index.php">Home</a></li>
+            <li><a id="home" href="all_user.php">All Users</a></li>
+            <li><a id="home" href="create_user.php">Create User</a></li>
+            <li><a id="home" href="transfer_money.php">Transfer Money</a></li>
+            <li><a id="home" href="transfer_log.php">Transaction Log</a></li>
+            <li><a id="about" href="#" target="blank">About me</a></li>
+            <li><a id="contact" href="contact.php">Contact</a></li>
+        </ul> 
+    </div>
+    <div class="menuicon">
+        <i id="menuicon" onclick="togglemenu()" class="fas fa-chevron-circle-down"> <span>MENU</span></i>
+    </div>
+</div>
+<div class="topspace"></div>' ;
+
+?>
